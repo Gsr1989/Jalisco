0d5c85185e76336eaf02f3f2d1e1b7e33c70a7d4
Gsr1989 <empresalorens@gmail.com>
2025-07-28 16:10:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -526,13 +526,5 @@ def registro_admin():
     # Petición GET normal (solo muestra el formulario)
     return render_template("registro_admin.html") 
 
-@app.route('/descargar_permiso/<folio>')
-def descargar_permiso(folio):
-    permiso_path = f"documentos/{folio}_jalisco1.pdf"
-    if not os.path.exists(permiso_path):
-        flash("Permiso no encontrado.", "error")
-        return redirect(request.referrer or url_for('admin_folios'))
-    return send_file(permiso_path, as_attachment=True)
-
 if __name__ == '__main__':
     app.run(debug=True)

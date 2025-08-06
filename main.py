85cd982c8c40c1e238a89f3cc573afd7f1eb2724
Gsr1989 <empresalorens@gmail.com>
2025-08-05 18:15:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 from supabase import create_client, Client
 import fitz
 import os
